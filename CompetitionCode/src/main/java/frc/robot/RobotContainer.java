4cd2aca9efa50487102183b43867f30e0109373d
emreg22 <57997786+emreg22@users.noreply.github.com>
2020-02-10 15:07:01 +0000

    Update RobotContainer.java

--- CompetitionCode/src/main/java/frc/robot/RobotContainer.java
+++ CompetitionCode/src/main/java/frc/robot/RobotContainer.java
@@ -11,7 +11,6 @@ import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.commands.EnableShooterCommand;
 import frc.robot.subsystems.Shooter;
