41b438646c2b6e59b167fead8326c2ef6a714af7
LordSquishers <lordsquisherton@gmail.com>
2020-02-12 17:45:06 +0000

    added BUTTON_

--- CompetitionCode/src/main/java/frc/robot/Constants.java
+++ CompetitionCode/src/main/java/frc/robot/Constants.java
@@ -11,6 +11,12 @@ public final class Constants {
 
   // === ROBOT PORTS === //
 
+  // DRIVE MOTORS
+  public static final int FRONT_LEFT_DRIVE_MOTOR = 1;
+  public static final int BACK_LEFT_DRIVE_MOTOR = 3;
+  public static final int FRONT_RIGHT_DRIVE_MOTOR = 2;
+  public static final int BACK_RIGHT_DRIVE_MOTOR = 0;
+  
   // INTAKE MOTORS
   public static final int WHEEL_INTAKE_MOTOR = 0;
   
@@ -35,16 +41,6 @@ public final class Constants {
   public static final double DEPLOY_INTAKE_SPEED = 0.5;
   public static final double WHEEL_INTAKE_SPEED = 0.5;
 
-
-  // === ROBOT PORTS === //
-
-  // DRIVE MOTORS
-  public static final int FRONT_LEFT_DRIVE_MOTOR = 1;
-  public static final int BACK_LEFT_DRIVE_MOTOR = 3;
-  public static final int FRONT_RIGHT_DRIVE_MOTOR = 2;
-  public static final int BACK_RIGHT_DRIVE_MOTOR = 0;
-
-
    // === CONTROLLERS === //
 
   // CONTROLLERS
@@ -61,14 +57,14 @@ public final class Constants {
   public static final int RIGHT_TRIGGER_AXIS = 3;
 
   // CONTROLLER BUTTONS (for xbox controller)
-  public static final int A = 1;
-  public static final int B = 2;
-  public static final int X = 3;
-  public static final int Y = 4;
-  public static final int LB = 5;
-  public static final int RB = 6;
-  public static final int BACK = 7;
-  public static final int START = 8;
+  public static final int BUTTON_A = 1;
+  public static final int BUTTON_B = 2;
+  public static final int BUTTON_X = 3;
+  public static final int BUTTON_Y = 4;
+  public static final int LEFT_BUMPER = 5;
+  public static final int RIGHT_BUMPER = 6;
+  public static final int BACK_BUTTON = 7;
+  public static final int START_BUTTON = 8;
   public static final int LEFT_STICK_BUTTON = 9;
   public static final int RIGHT_STICK_BUTTON = 10;
   
